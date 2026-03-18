26ca59c5af8cdd7bd37e3fc9858a52d3d3e86814
ALINE SOUZA <ssouzaline88@gmail.com>
2026-03-18 11:59:17 -0300

    Atualizando pastas do projeto

---
+++ script.js
@@ -0,0 +1,52 @@
+async function buscar() {
+  const termo = document.getElementById("pesquisa").value;
+  const resultado = document.getElementById("resultado");
+
+  if (!termo) {
+    resultado.innerHTML = "<p>Ecreva aqui...</p>";
+    return;
+  }
+
+  resultado.innerHTML = "Carregando...";
+
+  try {
+    const resposta = await fetch(
+      `https://itunes.apple.com/search?term=${termo}&media=music&limit=8`
+    );
+
+    const dados = await resposta.json();
+
+    resultado.innerHTML = "";
+
+    if (!dados.results || dados.results.length === 0) {
+      resultado.innerHTML = "<p>Nenhuma música encontrada </p>";
+      return;
+    }
+
+    dados.results.forEach(musica => {
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <img src="${musica.artworkUrl100}">
+        <h3>${musica.trackName}</h3>
+        <p>${musica.artistName}</p>
+        <audio controls src="${musica.previewUrl}"></audio>
+      `;
+
+      resultado.appendChild(card);
+    });
+
+  } catch (erro) {
+    console.log(erro);
+    resultado.innerHTML = "<p>Erro ao buscar músicas 😢</p>";
+  }
+}
+
+// ENTER para buscar
+document.getElementById("pesquisa").addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    buscar();
+  }
+});
